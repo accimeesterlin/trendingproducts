0ddd2e61baee7ac9a4a68e597a30116f3ed10dc3
Esterling Accime <accimeesterlin@yahoo.com>
2022-03-22 01:21:12 -0700

    remove default user

--- src/pages/admin/dashboard/index.js
+++ src/pages/admin/dashboard/index.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { listProducts } from "@Libs/api-product";
-import { getUserByEmail } from "@Libs/api-user";
 import SidebarWithHeader from "@Components/sidebar";
 
 const Dashboardpage = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(async () => {
-    getUserByEmail("info@pgecom.com").then((data) => {
-      console.log("Data: ", data);
-    });
-    listProducts()
-      .then(({ data }) => setProducts(data?.listProducts?.items))
-      .catch(() => {
-        // Handle fetching nrts
-        // setTotalDonation(data?.listProducts.items);
-      });
+    try {
+      listProducts()
+        .then(({ data }) => setProducts(data?.listProducts?.items))
+        .catch(() => {
+          // Handle fetching nrts
+          // setTotalDonation(data?.listProducts.items);
+        });
+    } catch (error) {
+      console.log("Error searching for products");
+    }
   }, []);
 
   return (
